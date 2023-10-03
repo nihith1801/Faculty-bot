2bf7c7428eefa84aa9f8985916c173e78e5f49ab
nihith1801 <122144472+nihith1801@users.noreply.github.com>
2023-10-03 22:34:25 +0530

    Update Bot.py
    Update: Included basic UI created from Streamlit.

--- Bot.py
+++ Bot.py
@@ -1,31 +1,31 @@
+import streamlit as st
 from gpt4all import GPT4All
+import time
 
 # Specify the path to your pre-trained model
-model_dir = "fmodel.bin"
+model_dir = "path_to_the_model"
 
 # Instantiate GPT4All with your pre-trained model and disable downloads
-model = GPT4All(model_dir, allow_download=False)
+model = GPT4All(model_dir, allow_download=True)
 
-# Print the default system and prompt templates
-print("Default system template:", repr(model.config['systemPrompt']))
-print("Default prompt template:", repr(model.config['promptTemplate']))
-print()
+'Loading...'
+iteration=st.empty()
+bar=st.progress(0)
+for i in range(100):
+    iteration.text(f'Loading {i+1}')
+    bar.progress(i+1)
+    time.sleep(0.1)
+'Done'
 
-# Start a conversation with the bot
-while True:
-    # Get a message from the user
-    user_message = input("User: ")
-
-    # If the user types 'exit', end the conversation
-    if user_message.lower() == "exit":
-        break
-
-    # Generate a response to the user's message
+# Generate a response to the user's message
+def generate_response(user_message):
     with model.chat_session():
         response = model.generate(prompt=user_message, temp=0)
-        print("Session system template:", repr(model.current_chat_session[0]['content']))
-        print("Session prompt template:", repr(model._current_prompt_template))
-    
     # Print the assistant's response
-    print(f"Assistant: {response}")
+    st.write(f"Assistant: {response}")
+
+# Get a message from the user
+user_message = st.text_input("User:")
+if user_message:
+    generate_response(user_message)
 
